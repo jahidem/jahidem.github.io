e8e57ed45189da21d8478ee5abbdf972606eba95
Jahid Hossain <54525656+jahidem@users.noreply.github.com>
2022-04-25 00:34:14 +0600

    dd

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="./portfolio/style.css">
+    <title>Portfolio</title>
+    </head>
+    <body>
+        <?php require './portfolio/index.html' ?>
+    </body>
+</html>
