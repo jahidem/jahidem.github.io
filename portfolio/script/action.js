b7a9c87c690b8b16177ee3d71c491f676063db8d
Jahid Hossain <54525656+jahidem@users.noreply.github.com>
2022-04-25 23:05:17 +0600

    ff

--- portfolio/script/action.js
+++ portfolio/script/action.js
@@ -2,28 +2,28 @@ const float_btn = document.getElementById("float-btn");
 const nav = document.getElementById("nav-contain");
 
 var timer;
-float_btn.addEventListener("click",()=>{
-    
-    
-    float_btn.classList.add("float-btn-hide");
-    nav.classList.add("nav-appear");
-    timer = setTimeout(wanna_show_nav, 4000);
-    
+float_btn.addEventListener("click", () => {
+  float_btn.classList.add("float-btn-hide");
+  nav.classList.add("nav-appear");
+  timer = setTimeout(wanna_show_nav, 4000);
+
   //  document.querySelector(".container").style.filter = "blur(2px)";
 });
 
-var wanna_show_nav = () =>{
+var wanna_show_nav = () => {
   nav.classList.remove("nav-appear");
   float_btn.classList.remove("float-btn-hide");
   complete();
-}
+};
 
-const complete = () =>{
+const complete = () => {
   clearInterval(timer);
-  timer=null;
-}
+  timer = null;
+};
 
-nav.addEventListener("click",()=>{
+nav.addEventListener("click", () => {
   complete();
   timer = setTimeout(wanna_show_nav, 4000);
 });
+
+/**Skill */
